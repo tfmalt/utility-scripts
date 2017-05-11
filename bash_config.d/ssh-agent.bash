f16c601f03a6f5fd07485a7de7cfc070180971b4
Thomas Malt <tm@damaskus.local>
2017-05-11 13:27:31 +0200

    added general ssh-agent support

--- bash_config.d/ssh-agent.bash
+++ bash_config.d/ssh-agent.bash
@@ -3,6 +3,26 @@
 #
 # @author Thomas Malt
 #
+SSH_ENV="$HOME/.ssh/environment"
+
+function start_agent {
+  echo -n " - Initialising new SSH agent... "
+  /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+  echo succeeded
+  chmod 600 "${SSH_ENV}"
+  source "${SSH_ENV}" > /dev/null
+}
+
+# Source SSH settings, if applicable
+
+if [ -f "${SSH_ENV}" ]; then
+  source "${SSH_ENV}" > /dev/null
+  ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+    start_agent;
+  }
+else
+  start_agent;
+fi
 
 case $(setuptype) in
   laptop)
