93313abe064f58c47fededab1d66f4430db135d7
Thomas Malt <tm@.malt.no>
2017-10-21 20:49:53 +0200

    added powerline to profile

---
+++ bash_config.d/powerline.bash
@@ -0,0 +1,14 @@
+# -*- sh -*-
+# Setting up powerline for bash
+#
+
+if [ -x /usr/bin/powerline ]; then
+  /usr/bin/powerline-daemon -q
+  export POWERLINE_BASH_CONTINUATION=1
+  export POWERLINE_BASH_SELECT=1
+  source /usr/share/powerline/bindings/bash/powerline.sh
+  export BASH_POWERLINE_LOADED="yes"
+  echo " - powerline loaded for bash"
+else
+  unset BASH_POWERLINE_LOADED
+fi
--- config/profile_mac.bash
+++ config/profile_mac.bash
@@ -13,7 +13,7 @@ fi
 set -o vi
 
 # Setting the path
-PATH="/usr/local/bin:/usr/local/sbin:$PATH"
+PATH="/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 
 [ -d $HOME/bin ]            && PATH=$PATH:$HOME/bin
 [ -d /usr/local/mysql/bin ] && PATH=$PATH:/usr/local/mysql/bin
@@ -32,8 +32,9 @@ DIRCOLOR=1
 LANGUAGE="en_US.UTF-8"
 LC_CTYPE="en_US.UTF-8"
 LC_ALL="en_US.UTF-8"
+XDG_CONFIG_HOME=$HOME/.config
 
-export LANGUAGE EDITOR RSYNC_RSH TERM VISUAL DIRCOLOR
+export XDG_CONFIG_HOME LANGUAGE EDITOR RSYNC_RSH TERM VISUAL DIRCOLOR
 
 # Load bash functions
 if [ -d $HOME/.bash_functions.d ]; then
@@ -46,21 +47,11 @@ else
 fi
 
 # Load bash completion
-if (( $EUID == 0 )); then
-    # echo "Logged in as root: loading bash-completion"
-    if [ -f /usr/share/bash-completion/bash_completion ]; then
-        source /usr/share/bash-completion/bash_completion
-    elif [ -f /etc/bash_completion ]; then
-        source /etc/bash_completion
-    fi
+if [ -f /etc/bash_completion ]; then
+  source /etc/bash_completion
 fi
 
 if [ -d $HOME/.bash_completion.d ]; then
-  if [[ $(setuptype) == "windows" ]]; then
-    echo " - loading bash completions for windows"
-    source /etc/bash_completion
-  fi
-
   for FILE in $HOME/.bash_completion.d/*sh; do
     source $FILE
   done
@@ -80,7 +71,7 @@ fi
 case $(uname) in
     Linux)
         alias ls="ls --color=auto"
-        eval $(dircolors -b $HOME/.dircolors/dircolors.256dark)
+        # eval $(dircolors -b $HOME/.dircolors/dircolors.256dark)
         ;;
     Darwin)
         alias ls="ls -G"
@@ -97,56 +88,59 @@ alias rm="rm -v"                                  #we like to be verbose
 alias mv="mv -v"
 alias cp="cp -v"
 
-# Setting the different command prompts.
-GITBRANCH="\[\e[38;5;196m\]\$(__git_ps1 ' %s')\[\e[0m\]"
-case $(setuptype) in
-    root)
-        PS1="[\[\e[38;05;9m\]\u\[\e[0m\]@\[\e[38;05;9m\]\h:\w\[\e[1;0m\]] "
-        ;;
-    laptop)
-        PS1="\[\e[38;5;214m\][\[\e[38;5;228m\]\u\[\e[38;5;214m\]@\[\e[38;5;228m\]\h"
-        PS1="${PS1}\[\e[38;5;214m\]:\[\e[0m\]\w$GITBRANCH\[\e[38;5;214m\]]\[\e[0m\] "
-        ;;
-    lxc)
-        PS1="\[\e[38;5;65m\][\[\e[38;5;77m\]\u\[\e[38;5;65m\]@\[\e[38;5;77m\]\h"
-        PS1="${PS1}\[\e[38;5;65m\]:\[\e[0m\]\w$GITBRANCH\[\e[38;5;65m\]]\[\e[0m\] "
-        ;;
-    nrk-laptop)
-        PS1="\[\e[38;5;214m\][\[\e[38;5;228m\]tm\[\e[38;5;214m\]@\[\e[38;5;228m\]nrk"
-        PS1="${PS1}\[\e[38;5;214m\]:\[\e[0m\]\w$GITBRANCH\[\e[38;5;214m\]]\[\e[0m\] "
-        ;;
-    linux-server)
-        PS1="\[\033[38;5;45m\][\[\033[38;5;87m\]\u\[\033[38;5;45m\]"
-        PS1="${PS1}@\[\033[38;5;87m\]\h"
-        PS1="${PS1}\[\033[38;5;45m\]:\[\033[38;5;87m\]\w$GITBRANCH\[\033[38;5;45m\]]\[\033[0m\] "
-	;;
-    linux-virtual)
-        PS1="\[\e[38;5;14m\][\[\033[0m\]\u\[\033[0;36m\]@\[\033[0m\]\h"
-        PS1="${PS1}\[\033[0;36m\]:\[\033[0m\]\w$GITBRANCH\[\e[38;5;14m\]]\[\033[0m\] "
-        ;;
-    linux)
-        PS1="\[\033[1;32m\][\[\033[0m\]\u\[\033[0;36m\]@\[\033[0m\]\h"
-        PS1="${PS1}\[\033[0;36m\]:\[\033[0m\]\w$GITBRANCH\[\033[1;32m\]]\[\033[0m\] "
-	;;
-    linux-rpi)
-        PI=$'\u03C0'
-        SIGMA=$'\u03A3'
+if [ $BASH_POWERLINE_LOADED != "yes" ]; then
+  # Setting the different command prompts.
+  GITBRANCH="\[\e[38;5;196m\]\$(__git_ps1 ' %s')\[\e[0m\]"
+  case $(setuptype) in
+      root)
+          PS1="[\[\e[38;05;9m\]\u\[\e[0m\]@\[\e[38;05;9m\]\h:\w\[\e[1;0m\]] "
+          ;;
+      laptop)
+          PS1="\[\e[38;5;214m\][\[\e[38;5;228m\]\u\[\e[38;5;214m\]@\[\e[38;5;228m\]\h"
+          PS1="${PS1}\[\e[38;5;214m\]:\[\e[0m\]\w$GITBRANCH\[\e[38;5;214m\]]\[\e[0m\] "
+          ;;
+      lxc)
+          PS1="\[\e[38;5;65m\][\[\e[38;5;77m\]\u\[\e[38;5;65m\]@\[\e[38;5;77m\]\h"
+          PS1="${PS1}\[\e[38;5;65m\]:\[\e[0m\]\w$GITBRANCH\[\e[38;5;65m\]]\[\e[0m\] "
+          ;;
+      nrk-laptop)
+          PS1="\[\e[38;5;214m\][\[\e[38;5;228m\]tm\[\e[38;5;214m\]@\[\e[38;5;228m\]nrk"
+          PS1="${PS1}\[\e[38;5;214m\]:\[\e[0m\]\w$GITBRANCH\[\e[38;5;214m\]]\[\e[0m\] "
+          ;;
+      linux-server)
+          PS1="\[\033[38;5;45m\][\[\033[38;5;87m\]\u\[\033[38;5;45m\]"
+          PS1="${PS1}@\[\033[38;5;87m\]\h"
+          PS1="${PS1}\[\033[38;5;45m\]:\[\033[38;5;87m\]\w$GITBRANCH\[\033[38;5;45m\]]\[\033[0m\] "
+  	;;
+      linux-virtual)
+          PS1="\[\e[38;5;14m\][\[\033[0m\]\u\[\033[0;36m\]@\[\033[0m\]\h"
+          PS1="${PS1}\[\033[0;36m\]:\[\033[0m\]\w$GITBRANCH\[\e[38;5;14m\]]\[\033[0m\] "
+          ;;
+      linux)
+          PS1="\[\033[1;32m\][\[\033[0m\]\u\[\033[0;36m\]@\[\033[0m\]\h"
+          PS1="${PS1}\[\033[0;36m\]:\[\033[0m\]\w$GITBRANCH\[\033[1;32m\]]\[\033[0m\] "
+  	;;
+      linux-rpi)
+          PI=$'\u03C0'
+          SIGMA=$'\u03A3'
 
-        PS1="\[\033[38;5;162m\][\[\033[38;5;174m\]\u@"
+          PS1="\[\033[38;5;162m\][\[\033[38;5;174m\]\u@"
 
-        if [ $(hostname) == "pi" ]; then
-            PS1="${PS1}${PI}"
-        elif [ $(hostname) == "sigma" ]; then
-            PS1="${PS1}${SIGMA}"
-        fi
+          if [ $(hostname) == "pi" ]; then
+              PS1="${PS1}${PI}"
+          elif [ $(hostname) == "sigma" ]; then
+              PS1="${PS1}${SIGMA}"
+          fi
 
-        PS1="${PS1}\[\033[38;5;162m\]:\[\033[0m\]\w$GITBRANCH\[\033[38;5;162m\]]\[\033[0m\] "
-	;;
-    *)
-        PS1="\[\033[1;32m\][\[\033[0m\]\u\[\033[0;36m\]@\[\033[0m\]\h"
-        PS1="${PS1}\[\033[0;36m\]:\[\033[0m\]\w$GITBRANCH\[\033[1;32m\]]\[\033[0m\] "
-        ;;
-esac
-export PS1
+          PS1="${PS1}\[\033[38;5;162m\]:\[\033[0m\]\w$GITBRANCH\[\033[38;5;162m\]]\[\033[0m\] "
+  	;;
+      *)
+          PS1="\[\033[1;32m\][\[\033[0m\]\u\[\033[0;36m\]@\[\033[0m\]\h"
+          PS1="${PS1}\[\033[0;36m\]:\[\033[0m\]\w$GITBRANCH\[\033[1;32m\]]\[\033[0m\] "
+          ;;
+  esac
+  export PS1
+fi
 
 export BASH_PROFILE_LOADED="yes"
+echo ""
