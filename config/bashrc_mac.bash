a81fa5fdb979ba14977a06c8f9346dae5c0d13a0
Thomas Malt <thomas@malt.no>
2017-11-19 14:57:04 +0100

    local changes

--- config/bashrc_mac.bash
+++ config/bashrc_mac.bash
@@ -7,3 +7,7 @@ if [[ ! $BASH_PROFILE_LOADED ]]; then
     source $HOME/.bash_profile
 fi
 
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
