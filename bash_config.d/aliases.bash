225cc6e7f8c1e266039c9b4a2815023ce21e3cea
Thomas Malt <thomas@malt.no>
2019-03-07 19:05:08 +0100

    added aliases snippet

---
+++ bash_config.d/aliases.bash
@@ -0,0 +1,28 @@
+# -*- sh -*-
+# config snippet to setup paths in a managable way across systems
+#
+# @author Thomas Malt
+#
+
+case $(uname) in 
+    Linux)
+      alias ls="ls --color=auto"
+      ;;
+    Darwin)
+      alias ls="ls -G"
+      ;;
+esac
+
+case $(setuptype) in
+  linux-server)
+    ;;
+  windows)
+    alias dotnet="dotnet.exe" 
+    ;;
+esac
+
+alias vboxheadless="VBoxHeadless"
+alias week="date +'%A %d %B %k:%M:%S Week %W'"
+alias rm="rm -v"                                  #we like to be verbose
+alias mv="mv -v"
+alias cp="cp -v"
