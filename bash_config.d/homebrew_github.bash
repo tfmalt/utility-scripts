caff5ba6d721acdc4a756c2488be0a20185f9d2d
Thomas Malt <thomas@malt.no>
2019-03-08 13:26:06 +0100

    updated gitub token for homebrew

--- bash_config.d/homebrew_github.bash
+++ bash_config.d/homebrew_github.bash
@@ -1,8 +1,8 @@
 # -*- sh -*-
 # @author Thomas Malt
 #
-
-HOMEBREW_GITHUB_API_TOKEN="45ef92b6b2a85077d86e9e7f8e595012f08dc5e6"
+# HOMEBREW_GITHUB_API_TOKEN="45ef92b6b2a85077d86e9e7f8e595012f08dc5e6"
+HOMEBREW_GITHUB_API_TOKEN="a1543e0caed855e7b478ad6981c54136d1113a6d"
 case $(setuptype) in
     laptop)
         export HOMEBREW_GITHUB_API_TOKEN
