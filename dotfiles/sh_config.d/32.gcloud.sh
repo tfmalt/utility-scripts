b1d3ba0011bc21f4864e92173174aa6e74f6d86f
Thomas Malt <thomas@malt.no>
2019-03-14 13:13:58 +0100

    added stub to include gcloud tools when present

---
+++ dotfiles/sh_config.d/32.gcloud.sh
@@ -0,0 +1,28 @@
+# -*- sh -*-
+# 
+# Include gcloud functions, autocomplete and tools
+#
+# @author Thomas Malt
+#
+
+if [[ -e $(command -v gcloud) ]]; then
+  [ -t 0 ] && echo -e "$ICON_OK Found gcloud. Setting up completion."
+  case $(setuptype) in
+    macbook)
+      CASKPATH='/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk'
+      if [[ $SHELL == *zsh ]]; then
+        source $CASKPATH/path.zsh.inc
+        source $CASKPATH/completion.zsh.inc
+      else 
+        source $CASKPATH/path.bash.inc
+        source $CASKPATH/completion.bash.inc
+      fi
+      ;;
+    *)
+      [ -t 0 ] && echo -e "   $ICON_ERR Don't know path to include files for $(setuptype)"
+      ;;
+  esac
+else
+  [ -t 0 ] && echo -e "$ICON_ERR google-cloud-sdk not found. Skipping."
+fi 
+
