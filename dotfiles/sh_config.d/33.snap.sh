897390c5da2cd3782a9c9a37c653f6510e32b7a2
Thomas Malt <thomas@malt.no>
2020-05-27 22:25:56 +0000

    added snap/bin to path

---
+++ dotfiles/sh_config.d/33.snap.sh
@@ -0,0 +1,10 @@
+# -*- sh -*-
+# config snippet to setup paths in a managable way across systems
+#
+# @author Thomas Malt
+#
+
+if [[ -d /snap/bin ]]; then
+    PATH="$PATH:/snap/bin"
+fi
+export PATH
