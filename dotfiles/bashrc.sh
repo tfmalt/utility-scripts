4d42f584f6b1dec3d2eb417b477ae8aa214bdfc1
Thomas Malt <thomas@malt.no>
2020-11-02 15:55:54 +0100

    updating setup with latest configuration on win10 wsl2

--- dotfiles/bashrc.sh
+++ dotfiles/bashrc.sh
@@ -59,3 +59,7 @@ else
 fi
 
 export BASH_PROFILE_LOADED="yes"
+
+export NVM_DIR="/usr/local/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
