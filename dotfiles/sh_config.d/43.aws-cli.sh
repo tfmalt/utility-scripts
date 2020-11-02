0e2c922b57d1d9721045d467cb4923bf2b5e8e08
Thomas Malt <thomas@malt.no>
2020-11-02 15:55:54 +0100

    updating setup with latest configuration on win10 wsl2

--- dotfiles/sh_config.d/43.aws-cli.sh
+++ dotfiles/sh_config.d/43.aws-cli.sh
@@ -6,7 +6,7 @@
 
 case $(setuptype) in
   macbook)
-    if [ -x /usr/local/bin/aws_completer]
+    if [ -x /usr/local/bin/aws_completer]; then
       complete -C /usr/local/bin/aws_completer aws
       [ -t 0 ] && echo -e "$ICON_OK Found aws-cli. Adding Autocompletion"
     fi  
