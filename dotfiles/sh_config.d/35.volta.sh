684a3c0819cd15d258f23956658908ee3c4fdf6e
Thomas Malt <thomas@malt.no>
2021-05-23 19:53:32 +0200

    Added script to configure volta

---
+++ dotfiles/sh_config.d/35.volta.sh
@@ -0,0 +1,15 @@
+# -*- sh -*-
+# Config snippet to configure volta
+# @author Thomas Malt
+#
+
+VOLTA_HOME="$HOME/.volta"
+
+if [ -d "$VOLTA_HOME/bin" ]; then
+  [ -t 0 ] && echo -e "$ICON_OK Found $VOLTA_HOME/bin. Adding to path."
+  export PATH="$VOLTA_HOME/bin:$PATH"
+else
+  [ -t 0 ] && echo -e "$ICON_ERR $VOLTA_HOMt
+  E Not Found. Skipping."
+fi 
+
