5ce860e6588b0d02acae0c5e85d91424d78e9ed5
Thomas Malt <thomas@malt.no>
2021-06-28 09:43:13 +0200

    added snippet to configure sbanken

---
+++ dotfiles/sh_config.d/91.sbanken.sh
@@ -0,0 +1,10 @@
+# -*- sh -*-
+# Config snippet to read in credentials
+# @author Thomas Malt
+#
+
+if [ -f "$HOME/.sbanken" ]; then
+  source $HOME/.sbanken
+else 
+  [ -t 0 ] && echo -e "$ICON_ERR Sbanken config Not Found. Skipping."
+fi
