e4a8e0e4ee6434c1d0fe1916306f8f68701d8036
Thomas Malt <thomas@malt.no>
2021-07-02 09:42:02 +0200

    minor fixes

--- dotfiles/sh_config.d/40.ssh-agent.sh
+++ dotfiles/sh_config.d/40.ssh-agent.sh
@@ -7,35 +7,34 @@ SSH_ENV="$HOME/.ssh/environment"
 
 function start_agent {
   [ -t 0 ] && echo -ne "$ICON_INFO Initialising new SSH agent... "
-  /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+  /usr/bin/ssh-agent | sed 's/^echo/#echo/' >"${SSH_ENV}"
   [ -t 0 ] && echo -e "$ICON_OK succeeded"
   chmod 600 "${SSH_ENV}"
-  source "${SSH_ENV}" > /dev/null
+  source "${SSH_ENV}" >/dev/null
 
   case $(setuptype) in
-    macbook)
-      ssh-add -A 2>/dev/null
-      [ -t 0 ] && echo -e "$ICON_INFO Adding ssh identities found in keychain to ssh-agent."
-      ;;
-    windows)
-      [ -t 0 ] && echo -e "$ICON_INFO Adding ssh identities to ssh-agent: "
-      ssh-add
-      ;;
-    linux-rpi|root)
-      ;;
+  macbook)
+    ssh-add -A 2>/dev/null
+    [ -t 0 ] && echo -e "$ICON_INFO Adding ssh identities found in keychain to ssh-agent."
+    ;;
+  windows)
+    [ -t 0 ] && echo -e "$ICON_INFO Adding ssh identities to ssh-agent: "
+    ssh-add
+    ;;
+  linux-rpi | root) ;;
+
   esac
 }
 
 # Source SSH settings, if applicable
 
 if [ -f "${SSH_ENV}" ]; then
-  source "${SSH_ENV}" > /dev/null
+  source "${SSH_ENV}" >/dev/null
   if [[ $(ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$) ]]; then
     [ -t 0 ] && echo -e "$ICON_INFO ssh-agent aleady running ($SSH_AGENT_PID)"
   else
-    start_agent;
+    start_agent
   fi
 else
-  start_agent;
+  start_agent
 fi
-
