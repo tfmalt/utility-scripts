7ffec94dc4f0a08df4bad37fb0915073759a8d8c
Thomas Malt <thomas@malt.no>
2021-07-02 09:32:58 +0200

    minor changes

--- dotfiles/sh_config.d/35.volta.sh
+++ dotfiles/sh_config.d/35.volta.sh
@@ -9,7 +9,6 @@ if [ -d "$VOLTA_HOME/bin" ]; then
   [ -t 0 ] && echo -e "$ICON_OK Found $VOLTA_HOME/bin. Adding to path."
   export PATH="$VOLTA_HOME/bin:$PATH"
 else
-  [ -t 0 ] && echo -e "$ICON_ERR $VOLTA_HOMt
-  E Not Found. Skipping."
+  [ -t 0 ] && echo -e "$ICON_ERR $VOLTA_HOME Not Found. Skipping."
 fi 
 
