a9484ee2005c2dcccbb79dde9ec259272d2982d1
Thomas Malt <thomas@malt.no>
2021-07-02 09:32:58 +0200

    minor changes

--- dotfiles/sh_config.d/90.aliases.sh
+++ dotfiles/sh_config.d/90.aliases.sh
@@ -4,25 +4,26 @@
 # @author Thomas Malt
 #
 
-case $(uname) in 
-    Linux)
-      alias ls="ls --color=auto"
-      ;;
-    Darwin)
-      alias ls="ls -G"
-      ;;
+# lets see
+case $(uname) in
+Linux)
+  alias ls="ls --color=auto"
+  ;;
+Darwin)
+  alias ls="ls -G"
+  ;;
 esac
 
 case $(setuptype) in
-  linux-server)
-    ;;
-  windows)
-    alias dotnet="dotnet.exe" 
-    ;;
+linux-server) ;;
+
+windows)
+  alias dotnet="dotnet.exe"
+  ;;
 esac
 
 alias vboxheadless="VBoxHeadless"
 alias week="date +'%A %d %B %k:%M:%S Week %W'"
-alias rm="rm -v"                                  #we like to be verbose
+alias rm="rm -v" #we like to be verbose
 alias mv="mv -v"
 alias cp="cp -v"
