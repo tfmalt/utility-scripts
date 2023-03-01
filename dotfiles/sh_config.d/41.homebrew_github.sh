a5b90c3dc53508c0844649f5d2ec99c4e6ca4511
Thomas Malt <thomas@malt.no>
2023-03-01 09:23:37 +0100

    uncomitted updates

--- dotfiles/sh_config.d/41.homebrew_github.sh
+++ dotfiles/sh_config.d/41.homebrew_github.sh
@@ -2,7 +2,7 @@
 # @author Thomas Malt
 #
 # HOMEBREW_GITHUB_API_TOKEN="45ef92b6b2a85077d86e9e7f8e595012f08dc5e6"
-HOMEBREW_GITHUB_API_TOKEN="a1543e0caed855e7b478ad6981c54136d1113a6d"
+HOMEBREW_GITHUB_API_TOKEN="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"
 
 case $(setuptype) in
     macbook)
