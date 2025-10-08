bedc89adfd7f68ed2a1d84f16e93428bcb437de6
Thomas Malt <thomas@malt.no>
2025-10-08 13:20:56 +0200

    updated p10k configuration

--- dotfiles/sh_config.d/90.p10k-config.sh
+++ dotfiles/sh_config.d/90.p10k-config.sh
@@ -3,8 +3,8 @@
 # Only for zsh sessions
 
 if [ -n "$ZSH_VERSION" ]; then
-  if [ -r "$HOME/.p10k.zsh" ]; then
+  if [ -r "$DOTFILES/p10k.zsh" ]; then
     # shellcheck disable=SC1090
-    . "$HOME/.p10k.zsh"
+    . "$DOTFILES/p10k.zsh"
   fi
 fi
