e5063234f8e9fc777ce9b0332f99d27844b1f298
Thomas Malt <thomas@malt.no>
2026-08-06 20:17:02 +0000

    fix: remove broken os_icon content expansion in p10k config

--- profile/p10k.zsh
+++ profile/p10k.zsh
@@ -187,8 +187,6 @@
   # OS identifier color.
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=232
   typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=7
-  # Custom icon.
-  # typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='⭐'
 
   ################################[ prompt_char: prompt symbol ]################################
   # Transparent background.
